e1b9f4f378e24c0b0f3b2439d628546b3abea354
safiabano <safiabano736@gmail.com>
2023-04-29 13:04:19 +0500

    login-Form

---
+++ js/assets/js/app.js
@@ -0,0 +1,46 @@
+// var myName="anas"
+// let userInfo=prompt("how old are you?", "22");
+// if(userInfo>=18){
+//     console.log("you are hire");
+// }else{
+//     console.log("you are not allowed")
+// }
+// let lang =prompt("enter your language?");
+// if(lang === "html"){
+// alert("you are hired");
+// }else if(lang === "css"){
+//     alert("great");
+// }else{
+//     alert("no allow");
+// };
+// let lang =prompt("enter your language");
+// if(lang === "java"){
+//     console.log("welcome java");
+//  }else if(lang === "css"){
+//         console.log("ajao css");
+//  } else if(lang=== "ecma"){
+//    console.log("wow");
+//  }
+//  else{ console.log("not come");
+// }
+
+// let firstName ="hello";
+// let lastName ="world";
+// console.log(firstName + " " +lastName);
+// let lang = prompt("enter your lang");
+// let lang2=prompt("enter your lang2")
+// if(lang==="html"|| lang2 ==="css"){
+//     alert("you are hired")
+// }else if(lang ==="java"|| lang2 === "ecma"){
+//    alert("good")
+// }
+// else{
+//    alert("you are not hired")
+// }
+
+// let num1 =3;
+// let num2 =6;
+// console.log("---> "+ num1);
+// console.log("---> "+ num2);
+
+
